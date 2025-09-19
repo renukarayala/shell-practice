66515ffded6b456e3da436f91546447d8ec222b9
Anil Kumar Rayala <arayala@quinstreet.com>
2025-09-18 19:42:07 -0500

    hello world text

---
+++ conversations.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Anil:: Renuka is a fabulous girl"
+echo "Renuka:: Always, Thank you"
+echo "Anil:: "my kids are my strngth"
+echo "Renuka:: I know you love your kids morethan anyone"
