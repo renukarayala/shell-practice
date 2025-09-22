f77ba32b4ed575fdae32165e32dd3a95c064f140
renu.rayala <arayala@quinstreet.com>
2025-09-21 20:38:53 -0500

    Variables var

---
+++ Variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+PERSON1=$1
+PERSON2=$2
+echo "PERSON1:: Renuka is a fabulous girl"
+echo "PERSON2:: Always, Thank you"
+echo "PERSON1:: my kids are my strngth"
+echo "PERSON2:: I know you love our kids morethan anyone"
+
