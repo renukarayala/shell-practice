0b2b234e2c150edb01585cd05141b51ae6009766
Renu Rayala <Renu.rayala@gmail.com>
2025-10-02 11:26:37 -0500

    with colors installation check packages

--- 15- colors for installation.sh
+++ 15- colors for installation.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 
-USERID=$(id -u)
+ USERID=$(id -u)  #The output is stored in variable USERID
 R="\e[31m"
 G="\e[32m"
 Y="\e[35m"
 N="\e[0m"
 
-if [ $USERID -ne 0 ]; then
+if [ $USERID -ne 0 ]; then  # If the user is not root (-ne 0), print an error and exit with code 1
     echo "ERROR:: Please run this script with root privelege"
     exit 1 # failure is other than 0
 fi
 
-VALIDATE(){ # functions receive inputs through args just like shell script args
-    if [ $1 -ne 0 ]; then
-        echo -e "Installing $2 ... $R FAILURE $N"
+VALIDATE(){ # Custom function to validate whether a command (like installation) was successful
+    if [ $1 -ne 0 ]; then  # $1: exit status ($?)
+        echo -e "Installing $2 ... $R FAILURE $N"  # -e means Escape codes work (colors, new lines, etc.) 
+        # $2: name of the package and If fail → print RED message otherwise normal
         exit 1
     else
-        echo -e "Installing $2 ... $G SUCCESS $N"
+        echo -e "Installing $2 ... $G SUCCESS $N"  # $2: name of the package and If success → print green message
     fi
 }
 
-dnf list installed mysql
+dnf list installed mysql # Check if installed
 # Install if it is not found
 if [ $? -ne 0 ]; then
-    dnf install mysql -y
-    VALIDATE $? "MySQL"
+    dnf install mysql -y  # If not installed
+    VALIDATE $? "MySQL" # Then call VALIDATE to print whether it succeeded or failed.
 else
-    echo -e "MySQL already exist ... $Y SKIPPING $N"
+    echo -e "MySQL already exist ... $Y SKIPPING $N" #If already installed, print a SKIPPING message (with colored output using $Y and $N
 fi
 
 dnf list installed nginx
