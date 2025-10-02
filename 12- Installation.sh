c167eaf65030401cd68e8cec4eef4fd16a7789ef
Renu Rayala <Renu.rayala@gmail.com>
2025-10-02 10:07:05 -0500

    installation

--- 12- Installation.sh
+++ 12- Installation.sh
@@ -5,6 +5,8 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
   echo "ERROR:: Please run this script with root privelege"
   exit 1  # failure is other than 0
+else
+    echo "worked with root"  
 fi
 dnf install nodejs -y
 
