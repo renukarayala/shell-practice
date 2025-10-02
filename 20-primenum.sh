2befcc7b29e49a3d4c12a8988b00bc27099137cf
Renu Rayala <Renu.rayala@gmail.com>
2025-10-02 13:10:11 -0500

    primenum

--- 20-primenum.sh
+++ 20-primenum.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Please enter number"
-read NUMBER
+read -p "Enter a number: " NUMBER
 
-if is_prime "$NUMBER"; then
+if [ $NUMBER ]; then
     echo "Given number is a Prime number"
 else
     echo "Given number is NOT a Prime number"
