15af14e5d864b5e2c78016e61b99a601a2f66ec6
Renu Rayala <Renu.rayala@gmail.com>
2025-10-02 10:08:51 -0500

    installation

--- 12- Installation.sh
+++ 12- Installation.sh
@@ -6,15 +6,8 @@ if [ $USERID -ne 0 ]; then
   echo "ERROR:: Please run this script with root privelege"
   exit 1  # failure is other than 0
 else
-    echo "worked with root"  
+    echo "working with root"  
 fi
-dnf install nodejs -y
-
-if [ $? -ne 0 ]; then
-    echo "ERROR:: Installing nodejs is failure"
-    exit 1
-else
-    echo "Installing nodejs is SUCCESS"
-fi
+dnf uninstall nodejs npm -y
 
 
