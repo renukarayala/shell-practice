b9c827ff2182289604e3264e35d9dc45288d6fce
Renu Rayala <Renu.rayala@gmail.com>
2025-10-02 13:26:32 -0500

    primenum

--- 20-primenum.sh
+++ 20-primenum.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-check_prime() 
-
-{
-
+check_prime() {
   NUMBER=$1
 
-  if [ $NUMBER -lt 100 ]; then
-   echo "$NUMBER is a Prime Number"
+  if [ "$NUMBER" -lt 100 ]; then
+    echo "$NUMBER is a Prime Number"
   else
     echo "$NUMBER is NOT a Prime Number"
   fi
