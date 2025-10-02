b09d27ef00ff12b735ac06ec53b3122ec3720216
Renu Rayala <Renu.rayala@gmail.com>
2025-10-02 12:54:41 -0500

    primenum

--- 20-primenum.sh
+++ 20-primenum.sh
@@ -2,16 +2,13 @@
 
 echo "Please enter number"
 read NUMBER
-NUMBER=$1-$100
-if [ $(($NUMBER % 2)) -nq 0 ]; then
-   echo "Given number is Prime number"
- else
-   echo "Given number is not prime number"  
 
-elif [ $NUMBER -lt 10 ]; then
-    echo "Given number $NUMBER is less than 10"
-elif [ $NUMBER -eq 10 ]; then
-    echo "Given number $NUMBER is equal to 10"
+if is_prime "$NUMBER"; then
+    echo "Given number is a Prime number"
 else
-    echo "Given number $NUMBER is greater than to 10"
+    echo "Given number is NOT a Prime number"
 fi
+
+
+
+
