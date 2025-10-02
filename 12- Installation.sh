c3b4517757d23dbe9ba59fd36e02599aa753a704
Renu Rayala <Renu.rayala@gmail.com>
2025-10-02 09:46:46 -0500

    installation

--- 12- Installation.sh
+++ 12- Installation.sh
@@ -2,11 +2,9 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ]; then
-    echo "ERROR:: Please run this script with root privelege"
-    exit 1 # failure is other than 0
+if [ $USERID -ne 0]; then
+  echo "ERROR:: Please run this script with root privelege"
 fi
-
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
